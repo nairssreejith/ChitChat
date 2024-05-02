7edffdfc1b30d88d53c1fea30c7732ac9b38bff2
Sreejith <sreejithsnair.sj@gmail.com>
2024-05-03 00:46:21 +0530

    Initial commit

---
+++ app/src/main/java/com/sreejithsnair/chitchat/ChitChatApplication.kt
@@ -0,0 +1,7 @@
+package com.sreejithsnair.chitchat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChitChatApplication : Application()
---
+++ app/src/main/java/com/sreejithsnair/chitchat/screens/StatusScreen.kt
@@ -0,0 +1,7 @@
+package com.sreejithsnair.chitchat.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StatusScreen() {
+}
